15934230137cb7d6134d4f7bc25685ba942f2b4d
Zach Bradford <zbradford1812@gmail.com>
2021-03-11 11:31:14 -0500

    completed first challenge

---
+++ addingApp.js
@@ -0,0 +1,18 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Give me the first number.', (number1) => {
+  rl.question('Give me the second number.', (number2) => {
+    let result = parseInt(number1) + parseInt(number2)
+    console.log(result)
+    rl.close()
+  })
+})
+
+ 
+
+
